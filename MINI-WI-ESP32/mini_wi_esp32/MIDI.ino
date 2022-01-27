a79fb0c011e6690f39e7f612d1ecafb9c800a5ae
Erich <35889973+ErichHeinemann@users.noreply.github.com>
2022-01-27 21:31:58 +0100

    Create MIDI.ino
    More or less a dummy-file. THe MIDI-Lib did not work well together with the BLE-Libs

---
+++ MINI-WI-ESP32/mini_wi_esp32/MIDI.ino
@@ -0,0 +1,28 @@
+byte last_channel=0;
+byte last_number=0;
+byte last_value=0;
+
+// Serial on Pin 16 RX / 17 TX
+// 16-Lila
+// 17 Blau
+
+
+// #define MYCHANNEL 0
+
+// void handleNoteOn
+
+/*void MIDI_setup(){
+ //  MIDI.begin();
+  // Register MIDI-Handlers
+  MIDI.setHandleNoteOn( handleNoteOn );
+  MIDI.setHandleNoteOff( handleNoteOff );
+  MIDI.setHandleControlChange( handleControlChange );
+  MIDI.setHandleProgramChange( handleProgramChange );
+  MIDI.setHandlePitchBend( handlePitchBend );
+  MIDI.setHandleClock( handleClock );
+  MIDI.setHandleStart( handleStart );
+  MIDI.setHandleStop( handleStop );
+  MIDI.setHandleContinue( handleContinue );
+  MIDI.setHandleContinue( handleContinue );
+}
+*/
